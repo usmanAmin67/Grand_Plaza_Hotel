e0a2f54ebc7a5604ac47a7bd3dffb9661789af6e
usmanAmin67 <80848212+usmanAmin67@users.noreply.github.com>
2021-03-17 22:57:33 +0500

    Add files via upload

---
+++ grand_plaza_hotel/assets/js/main.js
@@ -0,0 +1,132 @@
+$(document).ready(function () {
+    //  sidebar
+    $('#nav-icon4').click(function () {
+        $(this).toggleClass('open');
+        $('.navbar-nav').toggleClass('active');
+        $('.overlay').toggleClass('active');
+
+    })
+
+    $('.overlay').click(function () {
+        $('.navbar-nav').toggleClass('active');
+        $('.overlay').removeClass('active');
+        $('#nav-icon4').removeClass('open');
+
+    })
+
+    $('.close-nav').click(function () {
+        $('.navbar-nav').removeClass('active');
+        $('.overlay').removeClass('active');
+    })
+
+    $('.nav-link').click(function () {
+        $('.navbar-nav').removeClass('active');
+        $('.overlay').removeClass('active');
+        $('#nav-icon4').removeClass('open');
+
+    })
+
+    // vertical flip card
+
+    $('.flip-card-btn').click(function () {
+        $('.flipper').toggleClass('flip');
+        $('.flip-text1').toggleClass('active');
+        $('.flip-text2').toggleClass('active');
+
+    });
+
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1500, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+
+    // aos
+    AOS.init({
+        once: true
+    });
+
+
+
+    jQuery("#owl-one").owlCarousel({
+        center: false,
+        items: 1,
+        loop: true,
+        margin: 50,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1.25
+            }
+        }
+    });
+
+
+    jQuery("#owl-two").owlCarousel({
+        center: false,
+        items: 1,
+        loop: true,
+        margin: 50,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 2.25
+            }
+        }
+    });
+
+    // Custom Button
+    // $(".customNextBtn").click(function () {
+    //     owl.trigger("next.owl.carousel");
+    // });
+    // $(".customPreviousBtn").click(function () {
+    //     owl.trigger("prev.owl.carousel");
+    // });
+
+    
+
+});
+
+
+
+
